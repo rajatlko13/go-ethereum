dbaf9d4d295c44036fbd52c948e51e52e3a4d2c5
rajatlko13 <rajatlko13@gmail.com>
2021-11-17 01:59:50 +0530

    init script added

---
+++ initScript.sh
@@ -0,0 +1,7 @@
+rm -rf ~/geth-git/test-chain-dir
+
+geth --datadir ~/geth-git/test-chain-dir account import priv1
+geth --datadir ~/geth-git/test-chain-dir account import priv2
+geth --datadir ~/geth-git/test-chain-dir init genesis.json
+
+geth --datadir ~/geth-git/test-chain-dir console --rpcapi="db,eth,net,web3,personal,web3" --allow-insecure-unlock --http
