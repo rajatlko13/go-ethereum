491cda8122d4e431ac6f44330efc30ffbe062931
rajatlko13 <rajatlko13@gmail.com>
2021-11-17 02:18:01 +0530

    flag fixed

--- initScript.sh
+++ initScript.sh
@@ -4,4 +4,4 @@ geth --datadir ~/geth-git/test-chain-dir account import priv1
 geth --datadir ~/geth-git/test-chain-dir account import priv2
 geth --datadir ~/geth-git/test-chain-dir init genesis.json
 
-geth --datadir ~/geth-git/test-chain-dir console --rpcapi="db,eth,net,web3,personal,web3" --allow-insecure-unlock --http
+geth --datadir ~/geth-git/test-chain-dir console --http.api="db,eth,net,web3,personal,web3" --allow-insecure-unlock --http
