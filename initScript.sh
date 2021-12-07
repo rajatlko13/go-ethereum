490c2b066fe3e52c8b0369e43fee8fd50d936457
rajatlko13 <rajatlko13@gmail.com>
2021-12-07 10:25:25 +0530

    London fork block added

--- initScript.sh
+++ initScript.sh
@@ -4,4 +4,4 @@ geth --datadir ~/geth-git/test-chain-dir account import priv1
 geth --datadir ~/geth-git/test-chain-dir account import priv2
 geth --datadir ~/geth-git/test-chain-dir init genesis.json
 
-geth --datadir ~/geth-git/test-chain-dir console --http.api="db,eth,net,web3,personal,web3" --allow-insecure-unlock --http --miner.gaslimit '20000000' --unlock 0x0766fd8a11485bb8c045919ac5a07c51b3d1696b --mine
+geth --datadir ~/geth-git/test-chain-dir console --http.api="db,eth,net,web3,personal,web3" --allow-insecure-unlock --http --miner.gaslimit '20000000' --unlock 0x0766fd8a11485bb8c045919ac5a07c51b3d1696b --mine --txpool.globalslots '100000000' --txpool.globalqueue '100000000' --txpool.accountqueue '100000000'
